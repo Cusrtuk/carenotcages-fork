d4afa7d395959c28c3fe436f2404327b618bb5ed
kevingweinberg <kevingweinberg@gmail.com>
2022-10-16 18:48:20 -0400

    Update animate.js

--- animate.js
+++ animate.js
@@ -4,14 +4,18 @@
 // tl.from(".first-title", { x: 100, duration: 2 });
 
 gsap.from(".first-title", {
-  scrollTrigger: ".first-title",
+  scrollTrigger: { trigger: ".first-title", scrub: true },
   y: -50,
   opacity: 0,
-  duration: 1,
+  //   duration: 1,
 });
 
 gsap.from(".title-info", {
-  scrollTrigger: ".title-info",
+  scrollTrigger: {
+    trigger: ".title-info",
+    // scrub: true,
+    start: "center bottom",
+  },
   y: -50,
   opacity: 0,
   delay: 2,
@@ -31,6 +35,7 @@ gsap.from(".fade1", {
 gsap.from(".fade2", {
   scrollTrigger: {
     trigger: ".fade2",
+    // scrub: true,
     toggleActions: "restart none none none",
   },
   y: -50,
@@ -51,6 +56,7 @@ gsap.from(".fade3", {
 gsap.from(".fade4", {
   scrollTrigger: {
     trigger: ".fade4",
+    scrub: true,
     toggleActions: "restart none none none",
   },
   y: -50,
@@ -61,16 +67,18 @@ gsap.from(".fade4", {
 gsap.from(".sam", {
   scrollTrigger: {
     trigger: ".sam",
+    scrub: true,
     toggleActions: "restart none none none",
   },
   y: -100,
   opacity: 0,
-  duration: 1.5,
+  //   duration: 1.5,
 });
 
 gsap.from(".study-left", {
   scrollTrigger: {
     trigger: ".study-left",
+    // scrub: true,
     toggleActions: "restart none none none",
   },
   x: -150,
@@ -81,6 +89,8 @@ gsap.from(".study-left", {
 gsap.from(".study-right", {
   scrollTrigger: {
     trigger: ".study-right",
+    // scrub: true,
+    // start: "top center",
     toggleActions: "restart none none none",
   },
   x: 150,
